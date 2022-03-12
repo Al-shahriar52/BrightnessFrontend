d90ac616e50ee0d016bd29ceb7a2b5b134057e8c
Al-shahriar52 <shahriar15-2646@diu.edu.bd>
2022-03-12 13:06:17 +0600

    redux last

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React , {useEffect,useState} from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch,useSelector } from 'react-redux'
 import { Row, Col , Image, ListGroup,Button , Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import {listProductDetails} from '../actions/productActions'
+
 function ProductScreen({match}) {
-    const product=products.find((p) => p._id == match.params.id)
+    const dispatch=useDispatch()
+    const productDetails=useSelector(state => state.productDetails)
+    const {loading,error,product}=productDetails
+  useEffect(() =>{
+    dispatch(listProductDetails(match.params.id))
+    
+
+  },[dispatch,match])
+
+  
   return (
     <div>
         <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+        {loading ? <Loader />
+            : error ? <Message variant='danger'>{error}</Message>
+              :
         <Row>
             <Col md={6}>
                 <Image src={product.image} alt={product.name} fluid/>
@@ -64,6 +80,7 @@ function ProductScreen({match}) {
                 </Card>
             </Col>
         </Row>
+        }
        
     </div>
   )
